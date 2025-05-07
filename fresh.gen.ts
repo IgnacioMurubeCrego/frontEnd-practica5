cf2bf06e836e24ab6c2b410b56bb379a27c4541d
Ignacio Murube <IgnacioMurubeCrego@users.noreply.github.com>
2025-05-07 08:47:32 +0200

    remove axios

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,9 +5,7 @@
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $index from "./routes/index.tsx";
-import * as $search from "./routes/search.tsx";
 import * as $PostsViewer from "./islands/PostsViewer.tsx";
-import * as $SearchForm from "./islands/SearchForm.tsx";
 import type { Manifest } from "$fresh/server.ts";
 
 const manifest = {
@@ -15,11 +13,9 @@ const manifest = {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
     "./routes/index.tsx": $index,
-    "./routes/search.tsx": $search,
   },
   islands: {
     "./islands/PostsViewer.tsx": $PostsViewer,
-    "./islands/SearchForm.tsx": $SearchForm,
   },
   baseUrl: import.meta.url,
 } satisfies Manifest;
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,15 +1,15 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
-import Axios from "https://esm.sh/axios@1.8.1";
 import { postCardInfo } from "../types.ts";
 import PostsViewer from "../islands/PostsViewer.tsx";
 
 export const handler: Handlers = {
   async GET(_req, ctx) {
-    const response = await Axios.get<{ data: { posts: postCardInfo[] } }>(
+    const response = await fetch(
       `https://back-p5-y0e1.onrender.com/api/posts`,
     );
 
-    const posts = response.data.data.posts;
+    const data = await response.json();
+    const posts = data.data.posts;
 
     return ctx.render(posts);
   },
